c229b5ae91a843127685acecba8fc043db176529
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-04-08 21:04:39 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Globals  - NO WINDOW!!!!
+
+// __dirname   - Path to current directory
+// __filename  - file name
+// require     - function to use modules (Common JS)
+// module      - info about current module (file)
+// process     - info about the environment where the program is being executed
+
+console.log(__dirname);
+console.log(__filename);
+console.log(process);
