fba77faab8fbc0f6cdfd59a89ab9aa57942be21c
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-04-10 20:09:55 -0700

    Finished file reading and writing, sync and async

---
+++ 11-fsAsync.js
@@ -0,0 +1,28 @@
+const { readFile, writeFile } = require("fs");
+
+readFile("./content/first.txt", "utf8", (error, result) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (error, result) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./content/result-async.txt",
+      `Here is the result : ${first}, ${second}`,
+      { flag: "a" },
+      (error, result) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
